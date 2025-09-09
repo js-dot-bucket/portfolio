bb05e01cef1674535a1432ef7d39bb223a3da4ec
shubham sharma <shubhamsh3099@gmail.com>
2025-09-09 05:49:44 +0000

    Complete home page

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,23 +1,26 @@
 import {Outlet, Scripts} from "react-router";
-import "./root.css";
+import "./styles/root.css";
+import favicon from "/favicon.png"
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href={favicon} />
       </head>
       <body>
+        <Update />
         <Navbar />
         <Outlet />
+        <Footer />
         <Scripts />
       </body>
     </html>
   );
 }
 
-function Navbar() {
+function Update() {
   return (
     <div className="update">
       <p> New Blog about Xor in any website threw new column attack just added check it out! </p>
@@ -25,8 +28,25 @@ function Navbar() {
   );
 }
 
+function Navbar() {
+  return (
+    <navbar>
+      <ul>
+        <li>Home</li>
+        <li>Research</li>
+        <li className="underline">Hire Me</li>
+      </ul>
+    </navbar>
+  );
+}
 
-
+function Footer() {
+  return (
+    <footer>
+      <p>*This website follows a rolling release model</p>
+    </footer>
+  );
+}
 
 
 
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,37 @@
+import "../styles/home.css";
+import profile from "/app/images/pic.png";
 
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <div className="homePage">
+      <div className="container">
+        <p className="heroText">~/shubham</p>
+        <p className="heroPara">
+I’m passionate about exploring technology especially the web. I research, experiment, and break things (on purpose) to understand how they work.
+        </p>
+        <p className="heroPara">
+This is where I document everything I learn raw insights, deep dives, and the occasional rant. If it’s tech and it sparks curiosity, you’ll find it here.
+        </p>
+        <div className="columnBox">
+          <div className="box1">
+            <ul>
+              <li>How to find xor in any website ...</li>
+              <li>How to find xor in any website ...</li>
+              <li>How to find xor in any website ...</li>
+              <li>How to find xor in any website ...</li>
+              <li>How to find xor in any website ...</li>
+            </ul>
+            <button>Explore more blogs &#8594; </button>
+          </div> 
+          <div className="box2">
+            <div className="placeholder">
+            </div>
+            <div className="bgPlaceholder">
+              <img src={profile} height="290px" alt="profile image"/>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
+  );
 }
